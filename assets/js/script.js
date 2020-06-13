4c7a222e1f8ed7a8dfcfe7d4327461230fc1e335
JoeL <jlawton1203@gmail.com>
2020-06-13 11:52:25 -0500

    added logic to build password based on user criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,16 +25,37 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   //passwordText.value IS THE PART OF THE HTML ELEMENT THAT WILL HOLD THE TEXT. WHATEVER YOU ASSIGN TO IT WILL BE DISPLAYED ON THE HTML TEXT AREA.
   passwordText.value = password;
-
-
 }
 
+//this function generates the new password based on the supplied criteria and appends each character to newPassword.
+//once the password is built, it returns it.
 var generatePassword = function() {
-  //var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var newPassword = "";
   for (i = 0; i < passwordCriteria.size; i++){
-    console.log(String.fromCharCode(97 + randomNumber(0, 25)));
+    var charType = randomNumber(1, 4);
+
+    //compare tye new charType and if it is a type of character the user wants, generate a random character from that character set.
+    if (charType === 1 && passwordCriteria.lowercase === 1) {
+      newPassword += genLowercase();
+      console.log(newPassword);
+    }
+    else if (charType === 2 && passwordCriteria.uppercase === 1) {
+      newPassword += genUppercase();
+      console.log(newPassword);
+    }
+    else if (charType === 3 && passwordCriteria.numeric === 1) {
+      newPassword += genNumeric();
+      console.log(newPassword);
+    }
+    else if (charType === 4 && passwordCriteria.special === 1) {
+      newPassword += genSpecial();
+      console.log(newPassword);
+    }
+    //if all else fails, decrement i to throw out this iteration through the loop and try again.
+    else i--;
+    
   }
-  return "password";
+  return newPassword;
 }
 
 var randomNumber = function(min, max) {
@@ -43,10 +64,12 @@ var randomNumber = function(min, max) {
   return value;
 }
 
+//this function presents the user with a series of prompts to determine what criteria the password 
+//should include. The criteria are stored in a global object to make it easier for all parts of the 
+//program to access the criteria.
 var criteriaPrompts = function() {
   passwordCriteria.size = window.prompt("How long should the password be? (8 - 128 characters)")
-  //console.log(passwordCriteria.size);
-  //Prompt for each type of characters to include and set the appropriate criteria to 1 if included.
+
   var lowercaseConfirm = window.confirm("Would you like to include lowercase letters?");
   if (lowercaseConfirm) {
     passwordCriteria.lowercase = 1;
@@ -64,6 +87,26 @@ var criteriaPrompts = function() {
     passwordCriteria.special = 1;
   }
   console.log(passwordCriteria);
+}
+
+//these functions generate a single character from the appropriate character set and return it.
+//NOTE: I did not incldue all possible special characters to simplify the code for the challenge.
+//The character sets are based on the ASCII table, I am assuming only standard english characters
+//also no emoji's or anything else in Unicode.
+var genLowercase = function() {
+  return String.fromCharCode(97 + randomNumber(0, 25));
+}
+
+var genUppercase = function() {
+  return String.fromCharCode(65 + randomNumber(0, 25));
+}
+
+var genNumeric = function() {
+  return String.fromCharCode(49 + randomNumber(0, 8));
+}
+
+var genSpecial = function() {
+  return String.fromCharCode(33 + randomNumber(0, 14));
 }
 
 // Add event listener to generate button (MAKES THE BUTTON ON THE HTML PAGE DO SOMETHING)
